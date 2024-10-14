ee14def67a76b29529c07d72b5849b939927b959
Simon Ashton <simonashton.dev@gmail.com>
2024-10-14 10:20:58 -0700

    Add extension argument

--- cp-tool.py
+++ cp-tool.py
@@ -2,25 +2,38 @@ import os
 import re
 import sys
 
-TEMPLATE_PATH = os.path.join(os.path.expanduser('~'), '.local/share/cp-tool/template.cpp')
+TEMPLATE_PATH = os.path.join(os.path.expanduser('~'), '.local/share/cp-tool/template')
+DEFAULT_EXTENSION = '.cpp'
 
 def main(args):
     cur_directory = os.getcwd()
 
+    # Check if args contains an extension
+    extension = DEFAULT_EXTENSION
+    if len(args) > 1 and (args[-1].startswith('-e=') or args[-1].startswith('--extension=')):
+        extension = '.' + args[-1].split('=')[1].lstrip('.')
+        args = args[:-1]
+    
+    # Check if template file exists before doing anything
+    full_template_path = TEMPLATE_PATH + extension
+    ensure_exists(full_template_path)
+
     # Command for creating contest files
     if len(args) > 3 and args[1] == 'contest' and args[2].isdigit():
-        new_dir = create_contest(cur_directory, int(args[2]), ' '.join(args[3:]))
+        num_problems = int(args[2])
+        contest_name = ' '.join(args[3:])
+        new_dir = create_contest(cur_directory, contest_name, num_problems, extension)
         print('Contest files created.')
         print_dir(new_dir)
     # Command for creating problem files
     elif len(args) > 2 and args[1] == 'problem':
-        new_dir = create_problem(cur_directory, ' '.join(args[2:]))
+        problem_name = ' '.join(args[2:])
+        new_dir = create_problem(cur_directory, problem_name, extension)
         print('Problem files created.')
         print_dir(new_dir)
     # Command for creating template
-    elif len(args) > 2 and args[1] == 'template':
-        file_path = args[2]
-        add_template(cur_directory, file_path, True)
+    elif len(args) == 3 and args[1] == 'template':
+        add_template(cur_directory, ' '.join(args[2:]), extension, True)
         print('Template file created')
     # Help command
     elif len(args) == 2 and args[1] == 'help':
@@ -30,28 +43,28 @@ def main(args):
         print('Invalid usage:')
         print_help()
 
-def create_contest(directory, num_problems, name):
+def create_contest(directory, num_problems, name, extension):
     # Create directory with problems: "problem1.cpp", "problem2.cpp", ...
     new_path = create_directory(directory, name)
     for i in range(1, num_problems+1):
         problem_name = 'problem' + str(i)
-        add_template(new_path, problem_name)
+        add_template(new_path, problem_name, extension)
     return new_path
 
-def create_problem(directory, name):
-    new_path = create_directory(directory, name)
-    add_template(new_path, name)
+def create_problem(directory, name, extension):
+    new_path = create_directory(directory, name, extension)
+    add_template(new_path, name, extension)
     return new_path
 
-def add_template(directory, file_name, override=False):
+def add_template(directory, file_name, extension, override=False):
     # Read template file
-    ensure_exists(TEMPLATE_PATH)
+    full_template_path = TEMPLATE_PATH + extension
     template_data = ""
-    with open(TEMPLATE_PATH, 'r') as f:
+    with open(full_template_path, 'r') as f:
         template_data = f.read()
 
-    formatted_file_name = format_name(file_name)
-    new_path = os.path.join(directory, formatted_file_name) + '.cpp'
+    formatted_file_name = format_name(file_name, extension)
+    new_path = os.path.join(directory, formatted_file_name) + extension
 
     # Write template file
     if not override:
@@ -64,20 +77,20 @@ def print_help():
     print('cpt problem <name>')
     print('cpt contest <num_problems> <name>')
     print('cpt template <name>')
+    print('--extension=<ext> or -e=<ext> to specify file extension')
 
-def create_directory(directory, directory_name):
-    formatted_directory_name = format_name(directory_name)
+def create_directory(directory, directory_name, extension):
+    formatted_directory_name = format_name(directory_name, extension)
     joined_path = os.path.join(directory, formatted_directory_name)
     ensure_not_exists(joined_path)
-
     os.mkdir(joined_path)
     return joined_path
 
-def format_name(name):
+def format_name(name, extension):
     # Replace spaces and remove special characters
     formatted = name.replace(' ', '_')
     formatted = re.sub(r'[^\w]', '', formatted)
-    formatted = formatted.rstrip('.cpp')
+    formatted = formatted.rstrip(extension)
     return formatted
 
 def print_dir(dir):
