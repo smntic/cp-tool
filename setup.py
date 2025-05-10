1b90397a651bdc031f0c674bd2a7c1cbe78768c2
Simon Ashton <simonashton.dev@gmail.com>
2025-05-10 10:01:02 -0700

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cptool-py',
-    version='1.1.3',
+    version='1.1.4',
     packages=find_packages(),
     scripts=['cpt', 'cp-tool.py'],
     install_requires=[],
